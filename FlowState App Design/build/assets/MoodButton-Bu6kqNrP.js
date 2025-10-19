05c23c8efc17c3dc84ad779fe37ddaea4906ff82
Gabrielle Boucher Cote <gabbybcote4@gmail.com>
2025-10-18 19:03:36 -0600

    Build output for Vercel

---
+++ FlowState App Design/build/assets/MoodButton-Bu6kqNrP.js
@@ -0,0 +1,5 @@
+import{j as o}from"./index-Dh3uFWtn.js";function e({label:t,color:s,selected:a=!1,onClick:n}){return o.jsx("button",{onClick:n,className:`
+        w-48 h-24 rounded-3xl transition-all duration-300 text-center
+        ${a?"scale-105 shadow-lg":"shadow-md hover:scale-102"}
+        ${s}
+      `,children:o.jsx("span",{className:`${a?"opacity-100":"opacity-80"}`,children:t})})}export{e as M};
